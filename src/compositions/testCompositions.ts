db884888e8885e39e1c6e899d213107a6711df4e
Rogacewicz, Maksymilian (External) <m.rogacewicz_external@wartsila.com>
2022-09-28 15:23:27 +0200

    dupa

--- src/compositions/testCompositions.ts
+++ src/compositions/testCompositions.ts
@@ -44,6 +44,34 @@ const composition1: Composition = {
       ],
       duration: 700,
     },
+    {
+      id: 5,
+      melody: [
+        { note: "C4", duration: 0.1, time: 0 },
+        { note: "D#4", duration: 0.1, time: 0.1 },
+        { note: "G#4", duration: 0.1, time: 0.2 },
+        { note: "C5", duration: 0.1, time: 0.3 },
+        { note: "D#4", duration: 0.1, time: 0.4 },
+        { note: "G4", duration: 0.1, time: 0.5 },
+        { note: "G#4", duration: 0.1, time: 0.6 },
+        { note: "B4", duration: 0.1, time: 0.7 },
+        { note: "C5", duration: 0.1, time: 0.8 },
+        { note: "G#4", duration: 0.1, time: 0.9 },
+      ],
+      duration: 1000,
+    },
+    {
+      id: 6,
+      melody: [
+        { note: "A#3", duration: 0.05, time: 0 },
+        { note: "D4", duration: 0.05, time: 0.05 },
+        { note: "F4", duration: 0.05, time: 0.1 },
+        { note: "A#4", duration: 0.05, time: 0.15 },
+        { note: "D5", duration: 0.05, time: 0.2 },
+        { note: "F5", duration: 0.05, time: 0.25 },
+      ],
+      duration: 300,
+    },
   ],
 };
 
